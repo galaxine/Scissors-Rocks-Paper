e1294d45a311a978444e4ec1a726a61d3cb98185
Serdar Özdemir <oezdemir50@gmail.com>
2021-09-21 19:40:35 +0200

    Implemented the functions needed to make this game playable.

--- app.js
+++ app.js
@@ -25,9 +25,9 @@
  * So the player and the cpu can earn points
  * 
  */
-
-let playerPoints = 0;
-let computerPoints = 0;
+/**
+ * i need to put them away, global sucks unless you have to
+ */
 
 function computerSelection() {
     let number = Math.ceil(Math.random() * 3);
@@ -35,8 +35,7 @@ function computerSelection() {
 }
 
 function playerSelection() {
-    let input;
-    input = (prompt("Make sure you are entering either rock, scissor or paper. upper or lower case doesn't really matter, just make sure you write it wit the correct lettering", "")).toLowerCase();
+    let input = (prompt("Make sure you are entering either rock, paper or scissor. Upper or lower case doesn't really matter, just make sure you write it with the correct spelling.", "")).toLowerCase();
     if (input === "rock") {
         return 1;
     } else if (input === "paper") {
@@ -46,36 +45,70 @@ function playerSelection() {
     }
 
 }
-
+/**
+ * compares the player's choice against the computer's choice.
+ * Rock = 1
+ * Scissors = 2
+ * Paper = 3
+ * returns 1, if the Computer wins, 2 if it's the player.
+ * Draw returns 0.
+ * 
+ * Also announces who won.
+ * 
+ * I am not sure if the strings would cause some weird "true false" magic, so i reverted to numbers.
+ * @param {Number} playerSelection 
+ * @param {Number} computerSelection 
+ * @returns Number
+ */
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection;
     let computer = computerSelection;
     if (player === 1 && computer === 2) {
-        console.log("Computer wins");
-        computerPoints++;
+        console.log("Rock got beaten by Paper! Computer wins");
+        return 1;
     } else if (player === 2 && computer === 1) {
-        console.log("Player wins");
-        playerPoints++;
+        console.log("Paper beats Rock! Player wins");
+        return 2;
     } else if (player === 2 && computer === 3) {
-        console.log("Computer wins");
-        computerPoints++;
+        console.log("Paper got beaten by Scissor! Computer wins");
+        return 1;
     } else if (player === 3 && computer === 2) {
-        console.log("Player wins");
-        playerPoints++;
+        console.log("Scissor beats Paper! Player wins");
+        return 2;
     } else if (player === 3 && computer === 1) {
-        console.log("Computer wins");
-        computerPoints++;
+        console.log("Scissor is beaten by Rock! Computer wins");
+        return 1;
     } else if (player === 1 && computer === 3) {
-        console.log("Player wins");
-        playerPoints++;
+        console.log("Rock beats Scissor! Player wins");
+        return 2;
+    }
+    console.log("DRAW!");
+    return 0;
+}
+
+function game() {
+    console.log("We are playing a game, wether you like it or not");
+    let playerPoints = 0;
+    let computerPoints = 0;
+    console.log("Let's play until one of us has won 3 rounds, okay?");
+    while (true) {
+        result = playRound(playerSelection(), computerSelection());
+        if (result === 1) {
+            computerPoints++;
+        } else if (result === 2) {
+            playerPoints++;
+        }
+        console.log("Player VS Computer");
+        console.log(playerPoints + " : " + computerPoints);
+        if (playerPoints === 3 || playerPoints === 3) {
+            break;
+        }
+    }
+    if (playerPoints === 3) {
+        alert("Player Won! You escaped the room unscathed! The event will accompany for a long time as a trauma");
     } else {
-        console.log("DRAW!")
+        alert("The Computer Won! You are losing your limbs and become a part of the wall's decoration!");
     }
 }
 
-
-for (let index = 0; index < 6; index++) {
-    playRound(playerSelection(), computerSelection())
-    console.log("player VS CPU");
-    console.log(playerPoints + " : " + computerPoints)
-}
+game();
